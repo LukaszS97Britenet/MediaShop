b9a8d79262f7e2d0e219e2d8bf0cb69acc0f1a55
LukaszS97Britenet <lukasz.sprawka@britenet.com.pl>
2021-08-30 23:18:16 +0200

    [Community] Submit Order

--- src/aura/MS_OrderSubmit/MS_OrderSubmitController.js
+++ src/aura/MS_OrderSubmit/MS_OrderSubmitController.js
@@ -1,5 +1,38 @@
 ({
     doInit: function(component, event, helper) {
         helper.getCart(component);
+    },
+
+    productDetails: function(component, event, helper) {
+        let ctarget = event.currentTarget;
+        let productid = ctarget.dataset.productid;
+        var navEvt = $A.get("e.force:navigateToSObject");
+        navEvt.setParams({
+          "recordId": productid,
+          "slideDevName": "related"
+        });
+        navEvt.fire();
+    },
+
+    showFirstScreen: function(component, event, helper) {
+        component.set("v.firstScreen", true);
+        component.set("v.secondScreen", false);
+        component.set("v.thirdScreen", false);
+    },
+
+    showSecondScreen: function(component, event, helper) {
+        component.set("v.firstScreen", false);
+        component.set("v.secondScreen", true);
+        component.set("v.thirdScreen", false);
+    },
+
+    showThirdScreen: function(component, event, helper) {
+        component.set("v.firstScreen", false);
+        component.set("v.secondScreen", false);
+        component.set("v.thirdScreen", true);
+    },
+
+    submitDetails: function(component, event, helper) {
+
     }
 })
