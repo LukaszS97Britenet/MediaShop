865218a0aca852e355295596559c296e82b6bbd9
LukaszS97Britenet <lukasz.sprawka@britenet.com.pl>
2021-09-01 11:05:25 +0200

    [Community] Order History

--- src/aura/MS_HistoryOrder/MS_HistoryOrderController.js
+++ src/aura/MS_HistoryOrder/MS_HistoryOrderController.js
@@ -12,5 +12,22 @@
           "slideDevName": "related"
         });
         navEvt.fire();
+    },
+
+    contactUs: function(component, event, helper) {
+       component.set("v.isOrderHistoryVisible", false);
+       let ctarget = event.currentTarget;
+       let orderId = ctarget.dataset.value;
+       var flow = component.find('flowData');
+       var inputVariables = [
+           {name : "orderId", type : "String", value: orderId}
+       ];
+       flow.startFlow('Case_Creation', inputVariables);
+    },
+
+    handleStatusChange : function (component, event) {
+       if(event.getParam("status") === "FINISHED") {
+            component.set("v.isOrderHistoryVisible", true);
+       }
     }
 })
